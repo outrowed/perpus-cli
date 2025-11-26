343214e956f8e869644c5af341ead3cad8ab38b0
Fhaive <120903826+Tiredaf1@users.noreply.github.com>
2025-11-26 14:58:58 +0700

    Switch to using namespace also

--- perpus/book.cpp
+++ perpus/book.cpp
@@ -1,45 +1,46 @@
+
 #include <string>
 #include <filesystem>
 #include <fstream>
 
 #pragma once
-
+using namespace std;
 struct Book {
-    std::string title;
-    std::string author;
+     string title;
+     string author;
     int publicationYear;
-    std::string isbn;
+     string isbn;
 };
 
-/* namespace fs = std::filesystem;
+/* namespace fs =  filesystem;
 
-bool bookExists(const fs::path& directory, const std::string& isbn) {
+bool bookExists(const fs::path& directory, const  string& isbn) {
     fs::path bookPath = directory / (isbn + ".txt");
     return fs::exists(bookPath);
 }
 
 void saveBook(const fs::path& directory, const Book& book) {
     fs::path bookPath = directory / (book.isbn + ".txt");
-    std::ofstream outFile(bookPath);
+     ofstream outFile(bookPath);
     if (outFile.is_open()) {
-        outFile << "Title: " << book.title << std::endl;
-        outFile << "Author: " << book.author << std::endl;
-        outFile << "Publication Year: " << book.publicationYear << std::endl;
-        outFile << "ISBN: " << book.isbn << std::endl;
+        outFile << "Title: " << book.title <<  endl;
+        outFile << "Author: " << book.author <<  endl;
+        outFile << "Publication Year: " << book.publicationYear <<  endl;
+        outFile << "ISBN: " << book.isbn <<  endl;
         outFile.close();
     }
 }
 
-void loadBook(const fs::path& directory, const std::string& isbn, Book& book) {
+void loadBook(const fs::path& directory, const  string& isbn, Book& book) {
     fs::path bookPath = directory / (isbn + ".txt");
-    std::ifstream inFile(bookPath);
+     ifstream inFile(bookPath);
     if (inFile.is_open()) {
-        std::getline(inFile, book.title);
-        std::getline(inFile, book.author);
-        std::string yearLine;
-        std::getline(inFile, yearLine);
-        book.publicationYear = std::stoi(yearLine.substr(yearLine.find(": ") + 2));
-        std::getline(inFile, book.isbn);
+         getline(inFile, book.title);
+         getline(inFile, book.author);
+         string yearLine;
+         getline(inFile, yearLine);
+        book.publicationYear =  stoi(yearLine.substr(yearLine.find(": ") + 2));
+         getline(inFile, book.isbn);
         inFile.close();
     }
 } */
